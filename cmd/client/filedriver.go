6964de14b56c2034575ddb10aa099445e853f23b
alexismrosales <aleisjorge91@gmail.com>
2024-10-27 23:33:47 -0600

    error managing refactored

--- cmd/client/filedriver.go
+++ cmd/client/filedriver.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"cmd/client/internal"
 	"log"
 	"os"
 )
 
 func main() {
-	app := RunApp()
+	// Here you can set your own path to save the logs of the APP, just
+	// add a your preferred path
+	logger, err := internal.NewLogger("~/.filedriver.log")
+	if err != nil {
+		log.Fatal("Error creating/reading loggerfile: ", err)
+	}
+	app := RunApp(logger)
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
