9f44e3daf6121231345aa70a6ffeedf48a6744f4
alexismrosales <aleisjorge91@gmail.com>
2024-11-13 17:47:34 -0600

    port args updated

--- cmd/fdserver/filedriver.go
+++ cmd/fdserver/filedriver.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/alexismrosales/FileDriver/pkg/logger"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/alexismrosales/FileDriver/pkg/logger"
+)
 
 const (
 	COMMANDPARSERCTX = "COMMAND PARSER"
@@ -8,15 +15,32 @@ const (
 )
 
 func main() {
-	server, err := NewServerUDP(askPort())
+	ip, port, err := askPort()
 	if err != nil {
-		return
+		panic(err)
+	}
+	fmt.Println("ip", ip, "port", port)
+	server, err := NewServerUDP(ip, port)
+	if err != nil {
+		panic(err)
 	}
 	server.logger.Print("Server created succesfully", internal.INFO)
 	defer server.conn.Close()
 	server.ListenPetitions()
 }
 
-func askPort() (string, int) {
-	return "127.0.0.1", 8080
+func askPort() (string, int, error) {
+	if len(os.Args) < 3 {
+		return "", 0, errors.New("Wrong args, you must assign the ip and port.")
+	}
+	args := os.Args
+	if len(args[2]) == 4 {
+		port, err := strconv.Atoi(args[2])
+		if err != nil {
+			return "", 0, errors.New("Invalid port")
+		}
+		return args[1], port, nil
+	}
+	// Returning default port
+	return "", 8080, errors.New("Direction not setted correctly")
 }
